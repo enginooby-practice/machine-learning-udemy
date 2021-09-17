89e2a618d389f7453542072ccd404d6e83270a96
unknown <enginoobz@gmail.com>
2021-09-17 09:41:37 +0200

    [Data Preprocessing] R

---
+++ 1-data-preprocessing/data_preprocessing_tools.R
@@ -0,0 +1,25 @@
+dataset <- read.csv('Data.csv')
+
+# Handle missing data
+dataset$Age[is.na(dataset$Age)] <- mean(dataset$Age, na.rm = T)
+dataset$Salary[is.na(dataset$Salary)] <- mean(dataset$Salary, na.rm = T)
+
+# Encode categorical data (into dummy vars)
+dataset$Country <- factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1, 2, 3))
+dataset$Purchased <- factor(dataset$Purchased,
+                           levels = c('No', 'Yes'),
+                           labels = c(0, 1))
+
+# Split dataset into training & test set
+#install.packages('caTools')
+library(caTools)
+set.seed(1)
+split <- sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set <- subset(dataset, split == T)
+test_set <- subset(dataset, split == F)
+
+# Scale numeric features
+training_set[, 2:3] <- scale(training_set[, 2:3])
+test_set[, 2:3] <- scale(test_set[, 2:3])
